28f5d7075ca60eff4ceb6ebf8977e9115e9fe6ef
ahmedbashir-repo <66797075+ahmedbashir-repo@users.noreply.github.com>
2020-06-21 14:59:26 +0500

    Added the tasks of assignment

--- src/Room.js
+++ src/Room.js
@@ -6,16 +6,22 @@ function Room(){
     // const setLit = state[1];
     const [isLit, setLit] = useState(true);
     let [age, setAge] = useState(22);
+    let [temp, setTemp] = useState(72)
     // function updateLit(){
     //     setLit(!isLit);   
  
     // }
     return (
         <div className = {`room ${isLit ? "lit" : "dark"}`}>
-            This is room component: {isLit ? "Lit" : "Dark"} <br/>
+            <h2>The room is: {isLit ? "Lit" : "Dark"} </h2>
+            <h2>Temperature is: {temp} &#x2109; </h2>
             The age is : {age}<br/>
-            <button onClick = {()=>setLit(!isLit)}>Toggle Light</button><br/>
-            <button onClick = {()=>{setAge(++age)}}>Increase Age</button>
+            <button className = "button" onClick = {()=>setLit(!isLit)}>Toggle Light</button><br/>
+            <button className = "button" onClick = {()=>setLit(true)}>On</button><br/>
+            <button className = "button" onClick = {()=>setLit(false)}>Off</button><br/>
+            <button className = "button incdec-button" onClick = {()=>setTemp(++temp)}>&oplus;</button><br/>
+            <button className = "button incdec-button" onClick = {()=>setTemp(--temp)}>&#8854;</button><br/>
+            <button  className = "button age-button" onClick = {()=>{setAge(++age)}}>Increase Age</button>
         </div>
     )
 }
