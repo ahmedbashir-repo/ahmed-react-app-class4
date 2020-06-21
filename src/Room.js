351df6bf84dbd36aa82309026f5f5a48e0c2e007
ahmedbashir-repo <66797075+ahmedbashir-repo@users.noreply.github.com>
2020-06-21 13:48:09 +0500

    Updated basic react app

---
+++ src/Room.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './Room.css';
+function Room(){
+    // const state = useState(true);
+    // const isLit = state[0];
+    // const setLit = state[1];
+    const [isLit, setLit] = useState(true);
+    let [age, setAge] = useState(22);
+    function updateLit(){
+        setLit(!isLit);   
+ 
+    }
+    return (
+        <div className = {isLit ? "lit" : "dark"}>
+            This is room component: {isLit ? "Lit" : "Dark"} <br/>
+            The age is : {age}<br/>
+            <button onClick = {updateLit}>Toggle Light</button><br/>
+            <button onClick = {()=>{setAge(++age)}}>Increase Age</button>
+        </div>
+    )
+}
+
+export default Room;
